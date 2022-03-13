be72968e943a6de0ec86a747fa89de2a0cc67646
eruc <eruc101010@gmail.com>
2022-03-13 13:41:37 +0800

    feat: keep the month as two characters

--- function/customizeFun.js
+++ function/customizeFun.js
@@ -3,7 +3,9 @@ function changeDateFormat(rowDate) {
   date.splice(0, 1)
   date.splice(3, 6)
   date.reverse().pop()
-  let month = `0${(rowDate.getMonth() + 1).toString()}`
+  let month = (rowDate.getMonth() + 1) < 9 ? //月份需要補0的機會比較大，設定為true的條件
+    `0${(rowDate.getMonth() + 1).toString()}` :
+    `${(rowDate.getMonth() + 1).toString()}`
   date.splice(1, 0, month)
   return date
 }
