38b130a3a85d7dd25cde9d279bfb55769844d4e1
eruc <eruc101010@gmail.com>
2022-04-01 23:03:48 +0800

    feat: add heroku css

--- function/constructor.js
+++ function/constructor.js
@@ -1,5 +1,9 @@
-function PageCss(name) {
-  this.css = `http://localhost:3000/stylesheets/${name}.css`
+function PageCss(name, env) {
+  if (env !== 3000) {
+    this.css = `https://apple-tart-01426.herokuapp.com/stylesheets/${name}.css`
+  } else {
+    this.css = `http://localhost:3000/stylesheets/${name}.css`
+  }
 }
 
 module.exports = {
