4da88b9709be4c522a36a1bd0e54eb037d7a0ff4
eruc <eruc101010@gmail.com>
2022-04-06 01:03:58 +0800

    feat: add reminder message

--- public/javascripts/resetPassword.js
+++ public/javascripts/resetPassword.js
@@ -4,5 +4,8 @@ const check = document.querySelector('.check')
 check.addEventListener('click', () => {
   if (verCode.value !== check.id) {
     event.preventDefault();
+    check.nextElementSibling.innerHTML = `
+    <h6>Verification Code is wrong!</h6>
+    `
   }
 })
