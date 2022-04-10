44cf2f3fde25e818b29be761a8f855de0bb8ee36
eruc <eruc101010@gmail.com>
2022-04-10 14:07:07 +0800

    feat: change heroku URL

--- function/constructor.js
+++ function/constructor.js
@@ -1,6 +1,6 @@
 function PageCss(name, env) {
   if (env !== 3000) {
-    this.css = `https://apple-tart-01426.herokuapp.com/stylesheets/${name}.css`
+    this.css = `https://exprense-tracker.herokuapp.com/stylesheets/${name}.css`
   } else {
     this.css = `http://localhost:3000/stylesheets/${name}.css`
   }
